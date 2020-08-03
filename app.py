d5a136452b3718c362cf768b72187fe2eb9d1635
Anthony <anthony.white2001@hotmail.com>
2020-08-03 19:53:46 +0000

    Added App.py recipe/breakfast functions, added breakfast & recipe htmls files, pulling recipes from Mongo

--- app.py
+++ app.py
@@ -3,22 +3,27 @@ from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'Recipe_Book'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/get_recipes')
+@app.route('/recipes')
 def get_recipes():
-    return render_template("recipes.html",
-                           recipes=mongo.db.recipes.find())
+    return render_template('breakfast.html',
+                           recipes=mongo.db.Recipes.find())
+
+
+@app.route('/recipe/<recipe_id>', methods=['GET', 'POST'])
+def recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    return render_template('recipe.html', recipe=the_recipe)
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+            port=os.environ.get('PORT'),
+            debug=False)
