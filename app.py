c9496318c96bf17db1f508debcafb355cf7de192
Anthony <anthony.white2001@hotmail.com>
2020-08-11 20:13:19 +0000

    Added about and add recipe page, recipes send data to mongo, spoke with mentor and altered code to filter categories straight from mongo instead of afterwards to increase speed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, request
+from flask import Flask, render_template, redirect, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -13,24 +13,43 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/breakfast')
 def breakfast():
+    recipes = mongo.db.Recipes.find({
+                               "Category_name": "Breakfast"})
     return render_template('breakfast.html',
-                           recipes=mongo.db.Recipes.find())
+                           recipes=recipes)
+
 
 @app.route('/lunch')
 def lunch():
+    recipes = mongo.db.Recipes.find({
+                               "Category_name": "Lunch"})
     return render_template('lunch.html',
-                           recipes=mongo.db.Recipes.find())
+                           recipes=recipes)
 
 
 @app.route('/dinner')
 def dinner():
+    recipes = mongo.db.recipes.find({
+                               "Category_name": "Dinner"})
     return render_template('dinner.html',
-                           recipes=mongo.db.Recipes.find())
+                           recipes=recipes)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/add_recipes', methods=['POST'])
+def add_recipes():
+    recipes = mongo.db.Recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('breakfast'))
+
+
+@app.route('/add_rep')
+def add_rep():
+    return render_template('add_recipe.html')
 
 
 @app.route('/recipe/<recipe_id>', methods=['GET', 'POST'])
